f9451bbd84af9c21598ba1c7e5448d5eba9cc066
Allison Boukall <boukall@cs.ualberta.ca>
2019-02-12 09:30:20 -0700

    try Travis testing

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals()) # bottom of the file
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    pass
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,20 @@
+from datetime import datetime
 from django.test import TestCase
+from .models import Question, Choice
 
 # Create your tests here.
+class PollTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(
+            question_text="test_question?",
+            pub_date=datetime.now()
+        )
+
+    def test_question_text_valid(self):
+        """Question text should work"""
+        question = Question.objects.get(
+            question_text="test_question?"
+        )
+        self.assertEqual(
+            question.question_text, "test_question?"
+        ).
